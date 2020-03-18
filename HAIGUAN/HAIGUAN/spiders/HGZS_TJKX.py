ef13b1829745c9f901ef07e80249742a56e565cb
sn_baby <sn_baby@qq.com>
2020-03-18 23:40:49 +0800

    xiugai

--- HAIGUAN/HAIGUAN/spiders/HGZS_TJKX.py
+++ HAIGUAN/HAIGUAN/spiders/HGZS_TJKX.py
@@ -4,8 +4,49 @@ import scrapy
 
 class HgzsTjkxSpider(scrapy.Spider):
     name = 'HGZS_TJKX'
-    allowed_domains = ['http://www.customs.gov.cn/customs/302249/302274/302275/index.html']
-    start_urls = ['http://http://www.customs.gov.cn/customs/302249/302274/302275/index.html/']
+    # allowed_domains = ['http://www.customs.gov.cn/customs/302249/302274/302275/index.html']
+    start_urls = ['http://http://www.customs.gov.cn/customs/302249/302274/302275/index.html']
+    custom_settings = {
+        # 并发请求
+        'CONCURRENT_REQUESTS': 10,
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 10,
+        'CONCURRENT_REQUESTS_PER_IP': 0,
+        # 下载暂停
+        'DOWNLOAD_DELAY': 0.5,
+        'ITEM_PIPELINES': {
+            # 设置异步入库方式
+            'HAIGUAN.pipelines.MysqlTwistedPipeline': 600,
+            # 去重逻辑
+            # 'investment_news.pipelines.DuplicatesPipeline': 200,
+        },
+        'DOWNLOADER_MIDDLEWARES': {
+            # 调用 scrapy_splash 打开此设置
+            # 'scrapy_splash.SplashCookiesMiddleware': 723,
+            # 'scrapy_splash.SplashMiddleware': 725,
+
+            # 设置设置默认代理
+            'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 700,
+            # 设置请求代理服务器
+            # 'HAIGUAN.util_custom.middleware.middlewares.ProxyMiddleWare': 100,
+            # 设置scrapy 自带请求头
+            'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+            # 自定义随机请求头
+            'HAIGUAN.util_custom.middleware.middlewares.MyUserAgentMiddleware': 120,
+            # 重试中间件
+            'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+            # 重试中间件
+            'HAIGUAN.util_custom.middleware.middlewares.MyRetryMiddleware': 90,
+        },
+        # 调用 scrapy_splash 打开此设置
+        'SPIDER_MIDDLEWARES': {
+            'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+        },
+        # 去重/api端口
+        # 'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
+        # # 'SPLASH_URL': "http://10.8.32.122:8050/"
+        'SPLASH_URL': "http://47.106.239.73:8050/"
+    }
 
     def parse(self, response):
+        logging.warning('正文获取失败,请检查')
         pass
