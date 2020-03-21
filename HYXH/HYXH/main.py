7f3134e58d55fac81932635617b0ed75f3560fd0
sn_baby <sn_baby@qq.com>
2020-03-21 22:39:33 +0800

    添加代码

--- HYXH/HYXH/main.py
+++ HYXH/HYXH/main.py
@@ -1,1 +1,1 @@
-# -*- coding: utf-8 -*-from  scrapy.cmdline import executeexecute('scrapy crawl SH_biomedical'.split())execute('scrapy crawl chengdu'.split())execute('scrapy crawl shenzhen'.split())execute('scrapy crawl ningbo'.split())
+# -*- coding: utf-8 -*-from  scrapy.cmdline import executeexecute('scrapy crawl SH_biomedical'.split())execute('scrapy crawl chengdu'.split())execute('scrapy crawl shenzhen'.split())execute('scrapy crawl ningbo'.split())execute('scrapy crawl shanghai'.split())
