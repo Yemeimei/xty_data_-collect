a9884185983c3dfd9dd7cde9e4bc21e6be431f76
sn_baby <sn_baby@qq.com>
2020-03-23 23:24:34 +0800

    xiugai

---
+++ HAIGUAN_DYNAMIC/HAIGUAN_DYNAMIC/items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class HaiguanDynamicItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    time = scrapy.Field()
+    content = scrapy.Field()
+    name = scrapy.Field()
+    website = scrapy.Field()
+    link = scrapy.Field()
+    create_time = scrapy.Field()
+    txt = scrapy.Field()
+    spider_name = scrapy.Field()
+    module_name = scrapy.Field()
