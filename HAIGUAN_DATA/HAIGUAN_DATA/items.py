2d749024f18b78b9972d946ae3ece169793db9d7
sn_baby <sn_baby@qq.com>
2020-03-27 13:56:21 +0800

    xiugai

---
+++ HAIGUAN_DATA/HAIGUAN_DATA/items.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class HaiguanDataItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    time = scrapy.Field()
+    content = scrapy.Field()
+    appendix = scrapy.Field()
+    name = scrapy.Field()
+    website = scrapy.Field()
+    link = scrapy.Field()
+    create_time = scrapy.Field()
+    appendix_name = scrapy.Field()
+    txt = scrapy.Field()
+    spider_name = scrapy.Field()
+    module_name = scrapy.Field()
